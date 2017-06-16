cc2f5dce161847132167fad50cee60f656f2a043
cuni0716 <rcuni8@gmail.com>
2017-06-16 18:54:11 +0200

    new strategy to use env vars

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import twitter from 'twitter';
 import moment from 'moment';
 
 
-require('dotenv').config();
-
 const TWITTER_CONSUMER_KEY = process.env.TWITTER_CONSUMER_KEY;
 const TWITTER_CONSUMER_SECRET = process.env.TWITTER_CONSUMER_SECRET;
 const TWITTER_ACCESS_TOKEN = process.env.TWITTER_ACCESS_TOKEN;
