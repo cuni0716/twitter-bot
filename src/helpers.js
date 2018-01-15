67e015ee38b4bcf0eb1e4ba842fd3b0c8b63b662
cuni0716 <rcuni8@gmail.com>
2018-01-15 11:52:16 +0100

    improve logging

--- src/helpers.js
+++ src/helpers.js
@@ -4,6 +4,7 @@ import addHours from 'date-fns/add_hours';
 import Retweet from './index';
 import { ONE_HOUR, THREE_HOURS } from './constants';
 
+
 export const calculateRating = (twit) => {
   if (twit.retweeted) return 0;
   const retweets = (twit.retweet_count || 0) * 2;
@@ -12,13 +13,15 @@ export const calculateRating = (twit) => {
 };
 
 export const time = () => format(addHours(new Date(), 1), 'DD-MM-YYYY HH:mm:ss');
+
 export const catchError = errors => console.error(time(), '--- [ ERROR ]', errors[0].message);
+
 export const randomTime = () => Math.floor((Math.random() * THREE_HOURS) + ONE_HOUR);
 
 export const prepareNewRetweet = () => {
   const delay = randomTime();
   const nextRetweetDate = new Date(Date.now() + delay);
   const formattedDate = format(addHours(nextRetweetDate, 1), 'HH:mm:ss');
-  console.log(`${time()} --- [ INFO ] Next retweet will be at ${formattedDate}\n`);
+  console.log(`${time()} --- [ INFO ] Next retweet will be at ${formattedDate}`);
   return setTimeout(Retweet, delay);
 };
